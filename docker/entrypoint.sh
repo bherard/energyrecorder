b722286f1c5828db7a3da80f60e04219c3365124
bherard <benoit.herard@orange.com>
2017-06-19 17:15:19 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 nohup /usr/local/energyrecorder/bin/run-webapp.sh &
 influxd &
 sleep 1
-curl https://raw.githubusercontent.com/bherard/energyrecorder/master/web.py/creation.iql|influx -ssl -unsafeSsl
+curl https://raw.githubusercontent.com/bherard/energyrecorder/master/web.py/creation.iql|influx
 ps aux|grep influxd|grep -v grep|awk '{print $2}'|xargs kill -9
 /usr/local/bin/create-certs.sh
 cat <<EOF >>i /etc/influxdb/influxdb.conf 
