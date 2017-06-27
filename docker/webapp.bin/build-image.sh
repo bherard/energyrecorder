ace09ec0a90a7034fba702ad4ee4b9a2ea139e67
Benoit HERARD <benoit.herard@orange.com>
2017-06-27 14:19:47 +0200

    Webapp bluider bash update

--- docker/webapp.bin/build-image.sh
+++ docker/webapp.bin/build-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#curl -s https://raw.githubusercontent.com/bherard/energyrecorder/master/docker/webapp.dockerfile |docker build -t energyrecorder/webapp -
-cat ../webapp.dockerfile |docker build -t energyrecorder/webapp -
+curl -s https://raw.githubusercontent.com/bherard/energyrecorder/master/docker/webapp.dockerfile |docker build -t energyrecorder/webapp -
+#cat ../webapp.dockerfile |docker build -t energyrecorder/webapp -
