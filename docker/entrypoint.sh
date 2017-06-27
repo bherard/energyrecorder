682ae3b07cf5f73a734e8db83e11979f10c50903
Benoit HERARD <benoit.herard@orange.com>
2017-06-27 14:09:33 +0200

    influx users management

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,13 +3,13 @@ nohup /usr/local/energyrecorder/bin/run-webapp.sh &
 influxd &
 sleep 1
 curl https://raw.githubusercontent.com/bherard/energyrecorder/master/web.py/creation.iql|influx
-echo "CREATE USER energymaster WITH PASSWORD '"$1"' WITH ALL PRIVILEGES"|influx
-echo "CREATE USER energyreader WITH PASSWORD '"$2"'"|influx
+echo "CREATE USER $1 WITH PASSWORD '"$2"' WITH ALL PRIVILEGES"|influx
+echo "CREATE USER $3 WITH PASSWORD '"$4"'"|influx
 echo "GRANT READ ON NRG TO energyreader"|influx
 
 ps aux|grep influxd|grep -v grep|awk '{print $2}'|xargs kill -9
 /usr/local/bin/create-certs.sh
-cat <<EOF >> /etc/influxdb/influxdb.conf 
+cat <<EOF >> /etc/influxdb/influxdb.conf
 
 [http]
   enabled = true
@@ -22,5 +22,4 @@ cat <<EOF >> /etc/influxdb/influxdb.conf
 	https-enabled = true
 	https-certificate = "/etc/ssl/influxdb.pem"
 EOF
-influxd 
-
+influxd
