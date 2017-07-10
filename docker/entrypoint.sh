0162536589fd75e0f82d74ea990ce5057f5a91ca
Benoit HERARD <benoit.herard@orange.com>
2017-07-10 08:44:23 +0200

    WebApp docker entrypoint bugfix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,27 +8,27 @@ if [ $? -ne 0 ] ; then
         echo "CREATE USER $1 WITH PASSWORD '"$2"' WITH ALL PRIVILEGES"|influx
         echo "CREATE USER $3 WITH PASSWORD '"$4"'"|influx
         echo "GRANT READ ON NRG TO energyreader"|influx
-        if [ ! -f /etc/ssl/influxdb.pem ] ; then
-                /usr/local/bin/create-certs.sh
-        fi
-        grep '[admin]' /etc/influxdb/influxdb.conf >/dev/null
-        if [ $? -ne 0 ] ; then
-                cat <<EOF >> /etc/influxdb/influxdb.conf
-
-[http]
-        enabled = true
-        auth-enabled = false
-        https-enabled = false
-        https-certificate = "/etc/ssl/influxdb.pem"
-
-[admin]
-        enabled = true
-        https-enabled = false
-        https-certificate = "/etc/ssl/influxdb.pem"
-EOF
-        fi
 else
         echo "Database already exists"
+fi
+if [ ! -f /etc/ssl/influxdb.pem ] ; then
+        /usr/local/bin/create-certs.sh
+fi
+grep '[admin]' /etc/influxdb/influxdb.conf >/dev/null
+if [ $? -ne 0 ] ; then
+        cat <<EOF >> /etc/influxdb/influxdb.conf
+
+[http]
+enabled = true
+auth-enabled = false
+https-enabled = false
+https-certificate = "/etc/ssl/influxdb.pem"
+
+[admin]
+enabled = true
+https-enabled = false
+https-certificate = "/etc/ssl/influxdb.pem"
+EOF
 fi
 ps aux|grep influxd|grep -v grep|awk '{print $2}'|xargs kill -9
 influxd
