c4318e2b69e18ad780cba998107adbbc54c5b41f
Benoit HERARD <benoit.herard@orange.com>
2017-07-10 08:28:40 +0200

    WebApp docker entrypoint bugfix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,24 +2,33 @@
 nohup /usr/local/energyrecorder/bin/run-webapp.sh &
 influxd &
 sleep 1
-curl https://raw.githubusercontent.com/bherard/energyrecorder/master/web.py/creation.iql|influx
-echo "CREATE USER $1 WITH PASSWORD '"$2"' WITH ALL PRIVILEGES"|influx
-echo "CREATE USER $3 WITH PASSWORD '"$4"'"|influx
-echo "GRANT READ ON NRG TO energyreader"|influx
-
-ps aux|grep influxd|grep -v grep|awk '{print $2}'|xargs kill -9
-/usr/local/bin/create-certs.sh
-cat <<EOF >> /etc/influxdb/influxdb.conf
+echo "show databases" | influx -username "$1" -password "$2"|grep NRG
+if [ $? -ne 0 ] ; then
+        curl https://raw.githubusercontent.com/bherard/energyrecorder/master/web.py/creation.iql|influx
+        echo "CREATE USER $1 WITH PASSWORD '"$2"' WITH ALL PRIVILEGES"|influx
+        echo "CREATE USER $3 WITH PASSWORD '"$4"'"|influx
+        echo "GRANT READ ON NRG TO energyreader"|influx
+        if [ ! -f /etc/ssl/influxdb.pem ] ; then
+                /usr/local/bin/create-certs.sh
+        fi
+        grep '[admin]' /etc/influxdb/influxdb.conf >/dev/null
+        if [ $? -ne 0 ] ; then
+                cat <<EOF >> /etc/influxdb/influxdb.conf
 
 [http]
-  enabled = true
-  auth-enabled = False
-  https-enabled = False
-	https-certificate = "/etc/ssl/influxdb.pem"
+        enabled = true
+        auth-enabled = false
+        https-enabled = false
+        https-certificate = "/etc/ssl/influxdb.pem"
 
 [admin]
-	enabled = true
-	https-enabled = False
-	https-certificate = "/etc/ssl/influxdb.pem"
+        enabled = true
+        https-enabled = false
+        https-certificate = "/etc/ssl/influxdb.pem"
 EOF
+        fi
+else
+        echo "Database already exists"
+fi
+ps aux|grep influxd|grep -v grep|awk '{print $2}'|xargs kill -9
 influxd
